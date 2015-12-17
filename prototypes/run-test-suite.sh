5b3eeba8aa2b48b59cd135f1a51c3d7a1d900ce5
Andor Penzes <andor.penzes@gmail.com>
2015-12-17 21:33:11 +0100

    Add profiling option to the run-test-suite.sh

--- prototypes/run-test-suite.sh
+++ prototypes/run-test-suite.sh
@@ -1,5 +1,19 @@
-#! /bin/sh
+#!/bin/bash
 
-cabal run lambdacube-compiler-test-suite $@
-./create-test-report.sh
-rm lambdacube-compiler-test-suite.tix
+if [ "$1" == "--profile" ] ; then
+  shift
+  cabal clean
+  cabal sandbox delete
+  cabal sandbox init
+  cabal install --only-dependencies --enable-library-profiling --enable-executable-profiling
+  cabal configure --flags "profiling" --enable-library-profiling --enable-executable-profiling
+  cabal build
+  cabal run lambdacube-compiler-test-suite -- $@ +RTS -p
+  ./create-test-report.sh
+  rm lambdacube-compiler-test-suite.tix
+  cabal sandbox delete
+else
+  cabal run lambdacube-compiler-test-suite -- $@
+  ./create-test-report.sh
+  rm lambdacube-compiler-test-suite.tix
+fi
