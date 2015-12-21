bb483d60fbdaf749044ea34d3b96da8eb0bd9541
Csaba Hruska <csaba.hruska@gmail.com>
2015-12-21 12:58:41 +0100

    separate ddl into lambdacube-ir repo

--- ddl/test/idHs.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE ScopedTypeVariables  #-}
-import qualified Data.ByteString.Lazy as B
-import Data.Aeson
-import IR
-
-main = do
-  Just (p :: Pipeline) <- decode <$> B.getContents
-  B.putStr $ encode p
