69650452f373514df079cd841f2dff414ba127df
Andor Penzes <andor.penzes@gmail.com>
2016-01-09 20:31:14 +0100

    separate coverage test suite

--- create-test-report.sh
+++ create-test-report.sh
@@ -6,7 +6,7 @@ HPC_DIR=dist/hpc/${CABAL_NAME}
 HPC_REPO_DIR=$HPC_DIR
 
 TEST_DIR=src/UnitTest
-TIX=lambdacube-compiler-test-suite.tix
+TIX=lambdacube-compiler-coverage-test-suite.tix
 
 hpc markup ${TIX} --hpcdir=${HPC_DIR} --destdir=${HPC_REPO_DIR}
 hpc report ${TIX} --hpcdir=${HPC_DIR} --per-module
--- run-test-suite.sh
+++ run-test-suite.sh
@@ -6,17 +6,28 @@ if [ "$1" == "--profile" ] ; then
   cabal sandbox delete
   cabal sandbox init
   cabal install --only-dependencies --enable-library-profiling --enable-executable-profiling --constraint="indentation -trifecta"
-  cabal configure --flags "profiling" --enable-library-profiling --enable-executable-profiling
+  cabal configure --flags "profiling coverage" --enable-library-profiling --enable-executable-profiling
   cabal build
   set +e
   cabal run lambdacube-compiler-unit-tests
   RESULT_UNITTESTS=`echo $?`
-  cabal run lambdacube-compiler-test-suite -- -r $@ +RTS -p
+  cabal run lambdacube-compiler-coverage-test-suite -- -r $@ +RTS -p
   RESULT_TESTS=`echo $?`
   ./create-test-report.sh
   rm lambdacube-compiler-test-suite.tix
   cabal sandbox delete
   cabal clean
+elif [ "$1" == "--coverage" ] ; then
+  shift
+  set +e
+  cabal install --only-dependencies -j1
+  cabal configure --flags "coverage"
+  cabal run lambdacube-compiler-unit-tests
+  RESULT_UNITTESTS=`echo $?`
+  cabal run lambdacube-compiler-coverage-test-suite -- -r $@
+  RESULT_TESTS=`echo $?`
+  ./create-test-report.sh
+  rm lambdacube-compiler-coverage-test-suite.tix
 else
   set +e
   cabal install --only-dependencies -j1
@@ -24,8 +35,6 @@ else
   RESULT_UNITTESTS=`echo $?`
   cabal run lambdacube-compiler-test-suite -- -r $@
   RESULT_TESTS=`echo $?`
-  ./create-test-report.sh
-  rm lambdacube-compiler-test-suite.tix
 fi
 
 if [[ $RESULT_UNITTESTS -ne 0 ]]; then
