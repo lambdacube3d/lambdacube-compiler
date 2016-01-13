4775db8d8914792a74ccca177cc77ddd65825fac
Andor Penzes <andor.penzes@gmail.com>
2016-01-13 16:11:13 +0100

    lc: load Prelude from cabal data dir

--- tool/Compiler.hs
+++ tool/Compiler.hs
@@ -1,22 +1,30 @@
+-- {-# LANGUAGE CPP #-}
 {-# LANGUAGE RecordWildCards #-}
 import Options.Applicative
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
 import LambdaCube.Compiler.Driver
+import System.FilePath
+import Paths_lambdacube_compiler (getDataDir)
 
 data Config
   = Config
   { srcName :: String
   , backend :: Backend
+  , sourceDir :: FilePath
   }
 
 sample :: Parser Config
 sample = Config
   <$> argument str (metavar "SOURCE_FILE")
   <*> flag OpenGL33 WebGL1 (long "webgl" <> help "generate WebGL 1.0 pipeline" )
+  <*> pure "/lc"
 
 main :: IO ()
-main = execParser opts >>= compile
+main = do
+  cabalDataDir <- getDataDir
+  cfg <- execParser opts
+  compile (cfg {sourceDir = cabalDataDir </> "lc"})
   where
     opts = info (helper <*> sample)
       ( fullDesc
@@ -25,7 +33,7 @@ main = execParser opts >>= compile
 
 compile :: Config -> IO ()
 compile Config{..} = do
-  pplRes <- compileMain ["."] backend srcName
+  pplRes <- compileMain [".", sourceDir] backend srcName
   case pplRes of
     Left err -> putStrLn err
     Right ppl -> do
