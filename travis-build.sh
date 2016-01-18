0b08b23492a5dad05dfbda3a94d485dab1e9e270
Andor Penzes <andor.penzes@gmail.com>
2016-01-18 14:48:59 +0100

    Add travis-build.sh

---
+++ travis-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+cd /root/source/lambdacube-compiler
+cabal check
+./run-test-suite.sh --coverage
+cabal sdist
+SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz && (cd dist && cabal install --force-reinstalls "$SRC_TGZ")
