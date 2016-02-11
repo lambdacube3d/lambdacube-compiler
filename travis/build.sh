5f79c96c397cb5e5668bf71c9095ddf1e379ebe6
Andor Penzes <andorp@users.noreply.github.com>
2016-02-11 14:17:05 +0100

    Remove the coverage testing on travis

--- travis/build.sh
+++ travis/build.sh
@@ -2,6 +2,6 @@
 
 cd /root/source/lambdacube-compiler
 cabal check
-./run-test-suite.sh --coverage
+./run-test-suite.sh
 cabal sdist
 SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz && (cd dist && cabal install --force-reinstalls "$SRC_TGZ")
