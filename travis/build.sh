7c5b5b4c6010e2ad7ff9a63c3ba8e9896e6ba506
Péter Diviánszky <divipp@gmail.com>
2016-02-11 22:46:02 +0100

    Revert "Remove the coverage testing on travis"
    This reverts commit 5f79c96c397cb5e5668bf71c9095ddf1e379ebe6.

--- travis/build.sh
+++ travis/build.sh
@@ -2,6 +2,6 @@
 
 cd /root/source/lambdacube-compiler
 cabal check
-./run-test-suite.sh
+./run-test-suite.sh --coverage
 cabal sdist
 SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz && (cd dist && cabal install --force-reinstalls "$SRC_TGZ")
