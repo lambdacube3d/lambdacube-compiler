d052fdc20bf4de1e5bf160ad94723f6e46654c4b
Csaba Hruska <csaba.hruska@gmail.com>
2016-11-07 12:46:40 +0100

    refactor cabal flags, by default only library and lc command line tool is built

--- run-test-suite.sh
+++ run-test-suite.sh
@@ -5,7 +5,7 @@ UNIT_TEST_PARAMS="--quickcheck-max-size 30 --quickcheck-tests 100"
 if [ "$1" == "--profile" ] ; then
   shift
   cabal install --only-dependencies --enable-library-profiling --enable-executable-profiling
-  cabal configure --flags "profiling onlytestsuite" --enable-library-profiling --enable-executable-profiling
+  cabal configure --flags "profiling testsuite -cli" --enable-library-profiling --enable-executable-profiling
   set +e
   RESULT_UNITTESTS=0
   cabal run lambdacube-compiler-test-suite -- -r -iperformance -i.ignore $@ +RTS -p
@@ -14,7 +14,7 @@ elif [ "$1" == "--coverage" ] ; then
   shift
   set +e
   cabal install --only-dependencies
-  cabal configure --flags "coverage"
+  cabal configure --flags "coverage alltest"
   cabal run lambdacube-compiler-unit-tests -- $UNIT_TEST_PARAMS
   RESULT_UNITTESTS=`echo $?`
   cabal run lambdacube-compiler-coverage-test-suite -- -iperformance -i.ignore -r $@
