d746f89dbec34c79b8c83fbde030b9394d46b408
Csaba Hruska <csaba.hruska@gmail.com>
2018-02-02 22:47:34 +0100

    fix ci build scripts

--- create-test-report.sh
+++ create-test-report.sh
@@ -8,5 +8,5 @@ HPC_REPO_DIR=$HPC_DIR
 TEST_DIR=src/UnitTest
 TIX=lambdacube-compiler-coverage-test-suite.tix
 
-hpc markup ${TIX} --hpcdir=${HPC_DIR} --destdir=${HPC_REPO_DIR}
-hpc report ${TIX} --hpcdir=${HPC_DIR} --per-module
+stack exec hpc -- markup ${TIX} --hpcdir=${HPC_DIR} --destdir=${HPC_REPO_DIR}
+stack exec hpc -- report ${TIX} --hpcdir=${HPC_DIR} --per-module
--- travis/build.sh
+++ travis/build.sh
@@ -2,7 +2,10 @@
 
 #cd /root/source/lambdacube-compiler
 #cabal check
+
 ./run-test-suite.sh --coverage
+
+stack sdist
+
 # TODO
-#cabal sdist
 #SRC_TGZ=$(cabal info . | awk '{print $2;exit}').tar.gz && (cd dist && cabal install --force-reinstalls "$SRC_TGZ")
